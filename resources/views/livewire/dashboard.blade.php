79fb56bda1cf28bb405563ade3054aa43963beeb
ritsuto10969er <rk14oor109@gmail.com>
2025-08-12 16:34:42 +0900

    refactor: adjust migrations

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -2,14 +2,32 @@
 
 use Livewire\Volt\Component;
 use App\Models\ListeningParty;
+use Livewire\Attributes\Validate;
 
 new class extends Component {
+
+    #[Validate('require|string|max:255')]
     public  string $name = '';
+
+    #[Validate('required')]
     public $startTime;
+
+    #[Validate('require|url')]
+    public string $mediaUrl = '';
 
     public function createListeningParty()
     {
+        $this->validate();
 
+        $episode = Episode::create([
+            'media_url' => $this->mediaUrl,
+        ]);
+
+        $listeningParty = ListeningParty::create([
+            'episode_id' => $episode->id,
+            'name'=> $this->name,
+            'start_time' => $this->startTime,
+        ]);
     }
 
     public function with()
@@ -24,8 +42,9 @@ new class extends Component {
     <div class="max-w-lg w-full px-4">
         <form wire:submit='crearteListeningParty' class="space-y-6">
             <x-input wire:model='name' placeholder="Listening Party Name" />
+            <x-input wire:model='mediaUrl' placeholder="Podcast Episode URL" description='Direct Episode Link or YouTube Link, RSS Feeds will grab the latest episode' />
             <x-datetime-picker wire:model='startTime' placeholder="Listening Party start Time" />
-            <x-button primary>Create Listening Party</x-button>
+            <x-button primary type='submit'>Create Listening Party</x-button>
         </form>
     </div>
 </div>
